7e94dcac11d85afb3c7f4a8c49712b9bee874705
Carlos-Zerda <czerda6@gmail.com>
2023-05-18 22:37:28 -0300

    carga github50

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://Carlos-Zerda.github.io/preentrega1-reactjs"
+  base:"https://carlos-zerda.github.io/preentrega1-reactjs/"
 })
